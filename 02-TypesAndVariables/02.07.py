3597a41aaf5e51b133af77d2bc60e866982e8079
Michał Witek <101933782+Tadzio43@users.noreply.github.com>
2022-10-22 11:42:52 +0200

    Add files via upload

---
+++ 02-TypesAndVariables/02.07.py
@@ -0,0 +1,12 @@
+n1 = 5
+n2 = 1
+n3 = 8
+n4 = 6
+n5 = 3
+print(n1+n2+n3+n4+n5)
+print(n1*n1+n2*n2+n3*n3+n4*n4+n5*n5)
+print(n3/n5)
+if n3 == n4:
+    print("The values are equal")
+else:
+    print("These values aren't equal")
